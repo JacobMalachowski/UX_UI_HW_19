a54d343ba4882cce39a697505711cb2f23094083
JacobMalachowski <jacobmalachowski@gmail.com>
2024-05-23 09:40:07 -0600

    modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,13 @@ console.log("wow this doesnt work");
 
 var qMark = document.getElementById("q-mark");
 var aboutMe = document.getElementById("about-me-container");
+var email = document.getElementById("email");
 
 qMark.addEventListener("click",function(){
   aboutMe.scrollIntoView({behavior:"smooth"});
+});
+
+email.addEventListener("click",function(event){
+  event.preventDefault();
+  window.open("mailto:jacobmalachowski@gmail.com");
 });
